9f690820ed812273bd86644d75ccc1dfb0a5e50a
engrsakib <snazmussakib01@gmail.com>
2025-02-21 14:29:48 +0600

    lodding update

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import { DNA, MagnifyingGlass } from 'react-loader-spinner';
+import React from "react";
+import { DNA, MagnifyingGlass, Watch } from "react-loader-spinner";
 
 const Loading = () => {
-    return (
-      <div className="flex justify-center items-center">
-        <DNA
-          visible={true}
-          height="80"
-          width="80"
-          ariaLabel="dna-loading"
-          wrapperStyle={{}}
-          wrapperClass="dna-wrapper"
-        />
-        
-      </div>
-    );
+  return (
+    <div className="flex justify-center items-center">
+      <Watch
+        visible={true}
+        height="80"
+        width="80"
+        radius="48"
+        color="#4fa94d"
+        ariaLabel="watch-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
+  );
 };
 
 export default Loading;
