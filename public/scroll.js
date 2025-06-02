c144337d0a122e42e94a23fa1a3392d023079b5d
engrsakib <snazmussakib01@gmail.com>
2025-06-02 16:37:22 +0600

    all file delete

--- public/scroll.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const body = document.querySelector("body");
-
-  body.style.scrollbarWidth = "thin"; // For Firefox
-  body.style.scrollBehavior = "smooth"; // Smooth scrolling for navigation
-});
